504b2d66cbe55dd5a802441a3162594ce6b69908
Ashish Patel <thescalion140@gmail.com>
2022-10-18 19:23:54 +0530

    api image checked

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react"
+import axios from 'axios';
+import React from 'react';
+import { BASE_URL, ACCESS_KEY, SECRET_KEY } from "./redux/Constants"
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const getImages = async () => {
+      let response = await axios.get(`${BASE_URL}photos?page=1/?client_id=${ACCESS_KEY}`)
+      // let response1 = await axios.post(`https://unsplash.com/oauth/token?client_id=${ACCESS_KEY}`)
+      response = await response.data
+      setData(response);
+    }
+    getImages();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span> unSplash</span>
+      {
+        data?.map((itm, index) => {
+          return (
+            <div key={index}>
+              <img src={itm.urls.small} alt='userImage' />
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
